c667eccb04b8f835ec1381a0ef6a990e6acc9797
Andy Martin <andymartin51@gmail.com>
2014-02-01 12:43:49 -0700

    stuff

--- app.js
+++ app.js
@@ -1,26 +1,66 @@
+var sets = [{
+	code: "DGM",
+	name: "Dragon's Maze"
+}, {
+	code: "GTC",
+	name: "Gatecrash"
+}, {
+	code: "M14",
+	name: "Magic 2014 Core Set"
+}, {
+	code: "RTR",
+	name: "Return to Ravnica"
+}, {
+	code: "THS",
+	name: "Theros"
+}];
+
 $(document).ready(function() {
-
-// 	$.ajax({
-// 		url: "AllSets-Standard.json",
-// 		error: function() {
-// 			console.log('error!');
-// 		},
-// 		success: function(data, textStatus, jqXHR) {
-// 			displaySets(data);
-// 		}
-// 	});
-
-$("#navTabs a:first").tab('show');
-
+	for (var i in sets) {
+		console.log(i);
+		$("#navTabs").append((parseInt(i) === 0 ? '<li class="active">' : '<li>') + '<a id="tab-' + sets[i].code + '" href="#' + sets[i].code + '" data-toggle="tab">'+sets[i].name+'</a></li>');
+		startLoadingSet(sets[i].code);
+	}
+	// $("#tab-"+sets[0].code).tab('show'); // part of the php stuff
 });
 
-// function displaySets(data) {
-// 	for (var key in data) {
-// 		$("#test").append("<li>" + key + "</li>");
-// 	}
-// }
+function startLoadingSet(set) {
+	$.ajax({
+		url: "AllSets-" + set + ".json",
+		dataType: "json",
+		error: function() {
+			console.log('Error. Check $.ajax() call.');
+		},
+		success: function(data) {
+			// console.log(data);
+			displaySet(data[set]);
+		}
+	})
+}
 
-function showCardModal(cardName,cardSet,cardImage) {
+function displaySet(data) {
+	// console.log(data);
+	var html = "<div class='tab-pane' id='"+data.code+"'>"+
+		"<table class='table table-condensed'><thead>"+
+		"<th class='card-thumbnail'>Thumbnail</th>"+
+		"<th class='card-name'>Name</th>"+
+		"<th class='card-cost'>Mana Cost</th>"+
+		"<th class='card-type'>Type</th>"+
+		"<th class='card-ptl'>P/T</th>"+
+		"<th class='card-rules'>Rules Text</th>"+
+		"<th class='card-flavor'>Flavor Text</th>"+
+		"<th class='card-rarity'>Set/Rarity</th>"+
+		'</thead><tbody id="tbody-'+data.code+'"></tbody></table>';
+	$("#tabContent").append(html);
+
+	for(var i in data.cards) {
+		var card = data.cards[i];
+		// console.log(card);
+		$("#tbody-"+data.code).append('<tr><td>Card '+i+'</td></tr>');
+	}
+}
+
+function showCardModal(cardName, cardSet, cardImage) {
 	$("#mTitle").html(cardName);
-	$("#cardImage").attr('src','images/sets/'+cardSet+'-original/'+cardImage+'.jpg');
+	$("#cardImage").attr('src', 'images/sets/' + cardSet + '-original/' + cardImage + '.jpg');
 }
--- index.php
+++ index.php
@@ -6,49 +6,52 @@
 <link rel="stylesheet" href="app.css"/>
 </head>
 <body>
-<?php $j = json_decode(file_get_contents("AllSets-Standard.json"),true); ?>
-<ul class="nav nav-tabs nav-justified" id="navTabs">
-	<?php foreach($j as $cardSet) { echo "<li><a href='#".$cardSet['code']."' data-toggle='tab'>".$cardSet['name']."</a></li>"; } ?>
+<?php //$j = json_decode(file_get_contents("AllSets-Standard.json"),true); ?>
+<ul class="nav nav-tabs" id="navTabs">
+	<?php //foreach($j as $cardSet) { echo "<li><a href='#".$cardSet['code']."' data-toggle='tab'>".$cardSet['name']."</a></li>"; } ?>
 </ul>
-<div class="tab-content">
-<?php 
-	$mtg_card = "";
-	foreach($j as $cardSet) { 
-		echo "<div class='tab-pane' id='".$cardSet['code']."'>".
-		"<table class='table table-condensed table-hover'><thead>".
-		"<th class='card-thumbnail'>Thumbnail</th>".
-		"<th class='card-name'>Name</th>".
-		"<th class='card-cost'>Mana Cost</th>".
-		"<th class='card-type'>Type</th>".
-		"<th class='card-ptl'>P/T</th>".
-		"<th class='card-rules'>Rules Text</th>".
-		"<th class='card-flavor'>Flavor Text</th>".
-		"<th class='card-rarity'>Set/Rarity</th>".
-		"</thead>".
-		"<tbody>";
-		foreach($cardSet['cards'] as $card) {
-			echo "<tr>";
-			echo "<td class='card-thumbnail'><a href='#' data-toggle='modal' data-target='#cardModal' onclick='showCardModal(\"".strtr($card['name'],array("Æ"=>"&AElig;"))."\",\"".$cardSet['code']."\",\"".$card['imageName']."\")'><img src='".str_replace("'","%27","images/sets/".$cardSet['code']."/".str_replace(" ","-",$card['imageName']).".jpg")."' /></a></td>";
-			echo isset($card['name']) ? "<td class='card-name'><a href='#' data-toggle='modal' data-target='#cardModal' onclick='showCardModal(\"".strtr($card['name'],array("Æ"=>"&AElig;"))."\",\"".$cardSet['code']."\",\"".$card['imageName']."\")'>".strtr($card['name'],array("Æ"=>"&AElig;"))."</a></td>" : "<td></td>";
-			echo isset($card['manaCost']) ? "<td>".strtr($card['manaCost'],array("{"=>"<img src='images/mana/","}"=>".jpg'/>","/W"=>"-W","/R"=>"-R","/B"=>"-B","/U"=>"-U","/G"=>"-G"))."<br /><br /><em>CMC: ".$card['cmc']."</em></td>" : "<td></td>";
-			echo isset($card['type']) ? "<td>".str_replace("—","-",$card['type'])."</td>" : "<td></td>";
-			if(isset($card['power'])) {
-				echo "<td style='text-align:center'>".$card['power']."/".$card['toughness']."</td>";
-			} elseif(isset($card['loyalty'])) {
-				echo "<td style='text-align:center'><b>&lsaquo;".$card['loyalty']."&rsaquo;</b></td>";
-			} else {
-				echo "<td></td>";
-			}
-			if(isset($card['text']) && strlen($card['text']) === 1) $card['text'] = "{T}: Add {".$card['text']."} to your mana pool.";
-			echo isset($card['text']) ? "<td class='card-rules-td'>".strtr($card['text'],array("{"=>"<img src='images/mana/","}"=>".jpg'/>","/W"=>"-W","/R"=>"-R","/B"=>"-B","/U"=>"-U","/G"=>"-G","—"=>"-","Æ"=>"&AElig;","\n"=>"<br />"))."</td>" : "<td></td>";
-			echo isset($card['flavor']) ? "<td class='card-flavor-td'><em>".strtr($card['flavor'],array("—"=>"-"))."</em></td>" : "<td></td>";
-			echo isset($card['rarity']) ? "<td style='text-align:center;'><img src='images/rarity/".$cardSet['code']."-".substr($card['rarity'],0,1).".jpg'/></td>" : "<td></td>";
-			echo "</tr>";
-		}
-		echo "</tbody>".
-		"</table>".
-		"</div>";
-	}
+<div class="tab-content" id="tabContent"></div>
+<?php
+	// function fixString($str) {
+	// 	return strtr($str,array("Æ"=>"&AElig;",'\''=>'&apos;',"—"=>"-"));
+	// }
+
+	// foreach($j as $cardSet) { 
+	// 	echo "<div class='tab-pane' id='".$cardSet['code']."'>".
+	// 	"<table class='table table-condensed'><thead>".
+	// 	"<th class='card-thumbnail'>Thumbnail</th>".
+	// 	"<th class='card-name'>Name</th>".
+	// 	"<th class='card-cost'>Mana Cost</th>".
+	// 	"<th class='card-type'>Type</th>".
+	// 	"<th class='card-ptl'>P/T</th>".
+	// 	"<th class='card-rules'>Rules Text</th>".
+	// 	"<th class='card-flavor'>Flavor Text</th>".
+	// 	"<th class='card-rarity'>Set/Rarity</th>".
+	// 	"</thead>".
+	// 	"<tbody>";
+	// 	foreach($cardSet['cards'] as $card) {
+	// 		echo "<tr>";
+	// 		echo "<td class='card-thumbnail'><a href='#' data-toggle='modal' data-target='#cardModal' onclick='showCardModal(\"".fixString($card['name'])."\",\"".$cardSet['code']."\",\"".fixString($card['imageName'])."\")'><img src='".strtr("images/sets/".$cardSet['code']."/".$card['imageName'].".jpg",array("'"=>"%27"," "=>"-"))."' /></a></td>";
+	// 		echo isset($card['name']) ? "<td class='card-name'><a href='#' data-toggle='modal' data-target='#cardModal' onclick='showCardModal(\"".fixString($card['name'])."\",\"".$cardSet['code']."\",\"".fixString($card['imageName'])."\")'>".fixString($card['name'])."</a></td>" : "<td></td>";
+	// 		echo isset($card['manaCost']) ? "<td>".strtr($card['manaCost'],array("{"=>"<img src='images/mana/","}"=>".jpg'/>","/W"=>"-W","/R"=>"-R","/B"=>"-B","/U"=>"-U","/G"=>"-G"))."<br /><br /><em>CMC: ".$card['cmc']."</em></td>" : "<td></td>";
+	// 		echo isset($card['type']) ? "<td>".fixString($card['type'])."</td>" : "<td></td>";
+	// 		if(isset($card['power'])) {
+	// 			echo "<td style='text-align:center'>".$card['power']."/".$card['toughness']."</td>";
+	// 		} elseif(isset($card['loyalty'])) {
+	// 			echo "<td style='text-align:center'><b>&lsaquo;".$card['loyalty']."&rsaquo;</b></td>";
+	// 		} else {
+	// 			echo "<td></td>";
+	// 		}
+	// 		if(isset($card['text']) && strlen($card['text']) === 1) $card['text'] = "{T}: Add {".$card['text']."} to your mana pool.";
+	// 		echo isset($card['text']) ? "<td class='card-rules-td'>".strtr($card['text'],array("{"=>"<img src='images/mana/","}"=>".jpg'/>","/W"=>"-W","/R"=>"-R","/B"=>"-B","/U"=>"-U","/G"=>"-G","—"=>"-","Æ"=>"&AElig;","\n"=>"<br />"))."</td>" : "<td></td>";
+	// 		echo isset($card['flavor']) ? "<td class='card-flavor-td'><em>".strtr($card['flavor'],array("—"=>"-"))."</em></td>" : "<td></td>";
+	// 		echo isset($card['rarity']) ? "<td style='text-align:center;'><img src='images/rarity/".$cardSet['code']."-".substr($card['rarity'],0,1).".jpg'/></td>" : "<td></td>";
+	// 		echo "</tr>";
+	// 	}
+	// 	echo "</tbody>".
+	// 	"</table>".
+	// 	"</div>";
+	// }
 ?>
 </div>
 
